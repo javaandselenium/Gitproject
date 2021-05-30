2509d04af62ff91912e217d514bf757bcc96ada0
QSP <QSP@QSP>
2021-05-30 13:39:44 +0530

    hello2

--- src/test/java/demo/Test1.java
+++ src/test/java/demo/Test1.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class Test1 {
 	@Test
 	public void demo() {
-		Reporter.log("hello selenium",true);
+		Reporter.log("hello java and selenium",true);
 	}
 
 }
